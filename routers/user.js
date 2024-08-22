303a895cadf211a97521e9bc777346d82e40be37
asrorbek7757 <sdeasrorbek@gmail.com>
2024-08-22 13:42:35 +0500

    salom dunyo

---
+++ routers/user.js
@@ -0,0 +1,13 @@
+const { Router } = require('express')
+
+const login = Router();
+
+const { getUser, Login, createUser, } = require('../controls/user')
+
+login.get('/getUser', getUser);
+login.post('/Login', Login);
+login.post('/createUser', createUser);
+
+
+
+module.exports = {login} 
