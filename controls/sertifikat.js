639d304de1a1ec32ddb40f04b6fd21a125b07df0
asrorbek7757 <sdeasrorbek@gmail.com>
2024-09-10 10:27:32 +0500

    123

--- controls/sertifikat.js
+++ controls/sertifikat.js
@@ -1,6 +1,10 @@
 const Sertificat = require('../model/SertifikatSchema');
-
-
+const token = process.env.TELEGRAM_TOKEN;
+const axios = require('axios');
+const fs = require('fs');
+const FormData = require('form-data');
+const bodyParser = require('body-parser');
+const TelegramBot = require('node-telegram-bot-api');
 // Foydalanuvchilarni olish funksiyasi
 const getSertificat = async (req, res) => {
     try {
@@ -94,8 +98,43 @@ const deleteSertifikat = async (req, res)=>{
         res.json({ seccess: true, message: error })
     }
 }
+const uploadSertifikat = async (req, res) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    const file = req.files.file;
+    const filePath = `${__dirname}/${file.name}`;
+
+    file.mv(filePath, async (err) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+
+        try {
+            const formData = new FormData();
+            formData.append('document', fs.createReadStream(filePath));
+
+            const response = await axios.post(`https://api.telegram.org/bot${token}/sendDocument`, formData, {
+                headers: formData.getHeaders(),
+                params: {
+                    chat_id: 6039225297 // O'zingizning chat_id ni qo'shing
+                },
+            });
+
+            console.log(response.data);
+            res.send('File uploaded and sent to telegram bot successfully!');
+        } catch (error) {
+            console.error('Error sending file to Telegram bot:', error);
+            res.status(500).send('Error sending file to Telegram bot.');
+        } finally {
+            fs.unlinkSync(filePath);
+        }
+    });
+}
 module.exports = {
     getSertificat,
     createSertificat,
-    deleteSertifikat
+    deleteSertifikat,
+    uploadSertifikat
 };
--- routes/sertifikat.js
+++ routes/sertifikat.js
@@ -2,10 +2,11 @@ const { Router } = require('express')
 
 const sertificat = Router();
 
-const { getSertificat, createSertificat, deleteSertifikat } = require('../controls/sertifikat')
+const { getSertificat, createSertificat, deleteSertifikat, uploadSertifikat } = require('../controls/sertifikat')
 
 sertificat.get('/getSertificat', getSertificat);
 sertificat.post('/createSertificat', createSertificat);
+sertificat.post('/uploadSertifikat', uploadSertifikat);
 sertificat.delete('/deleteSertifikat/:_id', deleteSertifikat);
 
 
